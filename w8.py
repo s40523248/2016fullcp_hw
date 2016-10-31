a58416041ed0c3633431c8ba06a126599592e9cd
scrum-1 <scrum-1@mde.tw>
2016-10-31 15:51:36 +0800

    48

---
+++ w8.py
@@ -0,0 +1,20 @@
+data3 = open("w6_group.txt").read()
+#print(data2)
+group = data3.splitlines()
+#print(group)
+#print(group[0])
+result_g=[]
+for i in group:
+    #print(i)
+    # print(i.split(",")[:-1])
+    for j in i.split(",")[:-1]:
+        result_g .append(j)
+print(result_g)
+
+data3 = open("w6_group.txt").read()
+registered= data3.splitlines()
+#print(registered )
+
+for m in registered:
+    if m not in result_g:
+        print(m)
