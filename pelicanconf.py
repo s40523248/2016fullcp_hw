c48171dabb21328054c4fce4c27c218e7052a63f
scrum-1 <scrum-1@mde.tw>
2016-11-14 13:25:38 +0800

    48

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '2016Fall CPA 課程網誌 (虎尾科大MDE)'
+AUTHOR = '40523248'
+SITENAME = '2016Fall CPB 課程網誌 (虎尾科大MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
